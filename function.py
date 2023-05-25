ed5117b61bb02df0ff5cc93c2cbf3041edbc95e8
tina <ghjk40319@gmail.com>
2023-05-25 10:26:41 +0800

    add function

--- function.py
+++ function.py
@@ -1,7 +1,28 @@
-def is_leap_year(year):
-	if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
-		retutn True
-	else:
-		return False
-a = int(input('請輸入年:'))
-print(is_leap_year(a))
+def wash(dry=False, water=8, dehydration=True):
+	print('加水',water,'分滿')
+	print('加洗衣精')
+	print('旋轉')
+	print('脫水')
+	if dry:
+		print('烘衣')
+
+wash(True,water=10) #使用function
+
+
+
+
+def add(x, y):
+	return x + y
+result = add(3, 4)
+print(result)
+
+def average(numbers):
+	return sum(numbers)/ len(numbers)
+	
+print (average([1, 2, 3]))
+print (average([1111, 2232, 456]))
+print (average([456, 789, 941]))
+
+
+
+
